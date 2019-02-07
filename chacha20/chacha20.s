0fb99cedd448e7930e2e96669907fb13792ed166
Sal Wolffs <sal.wolffs@gmail.com>
2019-02-07 23:51:06 +0100

    Wrote chacha20_core_asm header and setup

--- chacha20/chacha20.s
+++ chacha20/chacha20.s
@@ -1,4 +1,28 @@
-#TODO:write header, funcdef, and start
+.syntax unified
+.cpu cortex-m4
+
+.global chacha20_core_asm
+.type chacha20_core_asm, %function
+chacha20_core_asm:
+    push {r0-r13} # store args as well as everything we need to restore
+    ldm r1, {r10-r13} # load values from instream
+    mov r9, #5 # initialize loop counter
+    push {r9-r13} # counter and instream will share 2 regs total
+    # remember: the stack is full descending: push is STMDB
+    # additionally, lower numbered registers are always associated
+    # with lower addresses within a single push/pop/ldm/stm
+    add sp, #12
+    # so now we're pointing in the middle of the stack. Legend:
+    # +4 x13 
+    # sp x12 ^ IA
+    # -4 x15 v DB
+    # -8 x14 
+    # -12 loop counter
+    ldm r2, {r4-r11} # load keyvalues. They'll stay there during the loop
+    ldm r3, {r0-r3} # load constant. It'll also stay. 
+    # Note: Overwriting r3 like this is legal.
+    # r12 and r13 still contain *in+8 and *in+12. What luck: that's x12 and x13
+
 quadround:
     add r0, r4, rol #0
     eor r12, r0, ror #0
@@ -136,9 +160,9 @@ quadround:
     eor r5, r10, ror #5
 
     stmdb sp, {r12,r13} 
-    ldr r12, [sp, #8] # update and test the loop counter between switching.
+    ldr r12, [sp, #-12] # update and test the loop counter between switching.
     subs r12, #1
-    str r12, [sp, #8]
+    str r12, [sp, #-12]
     ldmia sp, {r12,r13} # now r12=x12, r13=x13
     add r1, r6, rol #25
     eor r12, r1, ror #8
