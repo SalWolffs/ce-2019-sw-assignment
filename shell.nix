e66ea003d943e579a42872f07d19f92e0bda3808
Lars Jellema <lars.jellema@gmail.com>
2019-02-11 23:34:34 +0100

    Set proper crossSystem for nix-shell

--- shell.nix
+++ shell.nix
@@ -1,9 +1,46 @@
-{ pkgs ? import <nixpkgs> {} }:
+let
+  crossSystem = {
+    config = "arm-none-eabihf";
+    libc = "newlib";
+    platform.gcc = {
+      cpu = "cortex-m4";
+    };
+  };
 
-with pkgs;
+  pkgs = import <nixpkgs> { inherit crossSystem; };
 
-stdenv.mkDerivation {
+  inherit (pkgs) stdenv;
+  inherit (pkgs.buildPackages) fetchFromGitHub python screen stlink;
+
+  libopencm3 = stdenv.mkDerivation {
+    name = "libopencm3";
+    src = fetchFromGitHub {
+      owner = "libopencm3";
+      repo = "libopencm3";
+      rev = "8064f6d0cbaca9719c25ee74af115f90deb2b3a0";
+      sha256 = "17m91skxbh1v59nl94wzwg7q29hagdg5vi891cjpa7zsv36vhc5j";
+    };
+
+    PREFIX = stdenv.hostPlatform.config;
+    TARGETS = "stm32/f4";
+
+    enableParellelBuilding = true;
+    nativeBuildInputs = [ python ];
+    preBuild = ''
+      patchShebangs scripts
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out
+      cp -r lib include $out
+      runHook postInstall
+    '';
+  };
+
+in stdenv.mkDerivation {
   name = "crypto-engineering";
-  nativeBuildInputs = [ gcc-arm-embedded-7 screen stlink python37 ]
-  ++ (with python37Packages; [ pyserial ]);
+  PREFIX = stdenv.hostPlatform.config;
+  OPENCM3_DIR = toString libopencm3;
+  nativeBuildInputs = [ screen stlink ];
 }
