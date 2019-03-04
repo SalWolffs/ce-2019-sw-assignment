194ded6f31e6104aa2e5a66f650fa67bddef2010
Lars Jellema <lars.jellema@gmail.com>
2019-03-04 20:50:36 +0100

    Finish poly1305 in assembly, to be tested

--- poly1305/poly1305.s
+++ poly1305/poly1305.s
@@ -10,8 +10,8 @@ crypto_onetimeauth_poly1305_asm:
 	@ r3     input stream length, upper half, assumed zero
 	@ [sp+0] key pointer, 32 bytes
 
-	push {r0,r4-r11,lr}
-	sub sp, #40
+	push {r0,r1,r4-r11,lr}
+	sub sp, #36
 	mov r14, r2
 
 	@ Convert key to 26-bit limbs
@@ -45,15 +45,48 @@ crypto_onetimeauth_poly1305_asm:
 	eor r8, r8
 	eor r9, r9
 
-loop:
-	@ TODO: Load bytes from in into r4r3r2r1r0
-	@ If inlen < 16, memcpy, then use new pointer, else set bit 129
+	@ r0-r4 scratch
+	@ r5-r9 accumulator polynomial
+	@ r14   loop counter
 
-	eor r0, r0
-	eor r1, r1
-	eor r2, r2
-	eor r3, r3
+loop:
+	@ If inlen < 16, memcpy, then use new pointer, else set bit 128
+
 	eor r4, r4
+	ldr r1, [sp, #40]               @ input stream pointer
+	add r3, r1, #16
+	subs r14, #16
+	ittt cc
+	strcc r3, [sp, #40]
+	movcc r4, #0x01000000
+	bcc fast_load
+
+	add r0, sp, #20
+	add r2, r14, #16
+	str r4, [r0]
+	str r4, [r0, #4]
+	str r4, [r0, #8]
+	str r4, [r0, #12]
+	bl memcpy
+	mov r1, r0
+	mov r0, #1
+	add r2, r1, r14
+	strb r0, [r2, #16]
+
+fast_load:
+	@ Load 16 bytes of input from memory at r1.
+	@ r4 may already contains the upper two bits, so don't clear it.
+	ldmia r1, {r0-r3}
+	mov r10, #0x03ffffff
+
+	orr r4, r4, r3, lsr #8
+	and r3, r10, r3, lsl #18
+	orr r3, r8, r2, lsr #14
+	and r2, r10, r2, lsl #12
+	orr r2, r2, r1, lsr #20
+	and r1, r10, r1, lsl #6
+	orr r1, r1, r0, lsr #26
+	and r0, r10
 
 	@ add26(h, c);
 	add r5, r0
@@ -142,10 +175,10 @@ loop:
 	and r5, r10, #0x03ffffff        @ r5     <   L
 	add r6, r6, r10, lsr #26        @ r6     <  2L
 
-	@ TODO: Check for loop end
+	bcc loop
 
 	@ Load upper half of key into 26-bit limbs.
-	ldr r10, [sp, #80]
+	ldr r10, [sp, #80]              @ Pointer to key, 32 bytes
 	add r10, #16
 	ldmia r10, {r0-r3}
 	mov r10, #0x03ffffff
@@ -219,7 +252,7 @@ loop:
 	orr r6, r5, r6, lsl #26
 
 	@ Store result in output
-	ldr r10, [sp, #40]
+	ldr r10, [sp, #36]
 	stmia r10, {r6-r9}
 
 	add sp, #44
