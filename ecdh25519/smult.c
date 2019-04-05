4d2481a3131e791f32995f678cf085972089d423
Lars Jellema <lars.jellema@gmail.com>
2019-04-05 18:01:08 +0200

    Fix wrong WINDOWSIZE usage

--- ecdh25519/smult.c
+++ ecdh25519/smult.c
@@ -18,18 +18,18 @@ int crypto_scalarmult(unsigned char *ss, const unsigned char *sk,
     if (group_ge_unpack(&k, pk))
         return -1;
 
-    group_ge multiples[WINDOWSIZE + 1];
+    group_ge multiples[WINDOWMASK + 1];
     multiples[0] = group_ge_neutral;
     multiples[1] = k;
 
-    for (i = 2; i < WINDOWSIZE + 1; i++) {
-        group_ge_add(multiples + i, multiples + i - 1, multiples + 1);
+    for (i = 2; i < WINDOWMASK + 1; i++) {
+        group_ge_add(multiples + i, multiples + i - 1, &k);
     }
 
     for (i = 31; i >= 0; i--) {
-        for (; j >= 0; j--) {
+        for (; j >= 0; j -= WINDOWSIZE) {
             group_ge_double(&k, &k);
-            group_ge_add_index(&k, &multiples, (t[i] >> j) & WINDOWMASK);
+            group_ge_add_index(&k, multiples, (t[i] >> j) & WINDOWMASK);
         }
         j = 7;
     }
